674c735475567bb45a93c3b8aa218933e3d35c25
ALFTavares <bob.mouse@gmail.com>
2020-04-15 00:46:22 +0100

    moved most stuff to Paths

--- Paths.cs
+++ Paths.cs
@@ -1,15 +1,150 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Doc_Merger
 {
-    public abstract class Paths
+    public class Paths
     {
-        public static List<string> AllFilePath { get; set; }
-        public List<string> FinalPath { get; set; }
-        public List<string> ErrorLogPath { get; set; }
+        #region Properties
+        /// <summary>
+        /// List containing all paths to files
+        /// </summary>
+        public List<string> AllFilesPath { get; set; }
+
+        /// <summary>
+        /// Document to be generated as result
+        /// </summary>
+        public string FinalDocument { get; set; }
+
+        /// <summary>
+        /// If some error occur in some file, the file path should be logged to this log file
+        /// </summary>
+        public string ErrorLog { get; set; }
+
+        /// <summary>
+        /// List containing all files
+        /// </summary>
+        public List<string> AllFiles;
+        #endregion
+
+        #region Constructors
+        public Paths(List<string> args)
+        {
+            AllFilesPath = args;
+            AllFiles = GetAllFiles();
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Validates if all paths and files are Ok to start
+        /// </summary>
+        /// <returns>false if any exception occurs while creating files and directories</returns>
+        public bool CreateAndValidatePaths()
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            string finalPath = currentDir + @"\Results";
+            string finalFile = finalPath + @"\Completed\Result.docx";
+            string errorFile = finalPath + @"\Errors\errors.txt";
+
+            try
+            {
+                CreateDirectoryIfNotExist($"{ currentDir }\\Results");
+                CreateDirectoryIfNotExist($"{ finalPath }\\Completed");
+                CreateDirectoryIfNotExist($"{ finalPath }\\Errors");
+
+                CreateFileIfNotExist(finalFile);
+                CreateFileIfNotExist(errorFile);
+
+                ErrorLog = errorFile;
+                FinalDocument = finalFile;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// execute the merge
+        /// </summary>
+        public void Execute()
+        {
+            var app = new Application();
+            var count = 1;
+
+            var finalFile = app.Documents.Open(FinalDocument);
+            finalFile.PageSetup.Orientation = WdOrientation.wdOrientLandscape;
+
+            AllFiles.ForEach(file =>
+            {
+                try
+                {
+                    finalFile.ActiveWindow.Selection.InsertFile(file);
+                    finalFile.Save();
+                    Console.WriteLine($"{count} - {file}\n");
+                }
+                catch (COMException)
+                {
+                    using (StreamWriter writer = File.CreateText(ErrorLog))
+                    {
+                        writer.WriteLine($"{file}\n");
+                    }
+                }
+                count++;
+            });
+
+            finalFile.Close();
+            app.Quit();
+        }
+
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Gets the files in all indicated directories
+        /// </summary>
+        /// <returns>a list containing all files</returns>
+        private List<string> GetAllFiles()
+        {
+            var files = new List<string>();
+
+            foreach (var filePath in AllFilesPath)
+            {
+                var filesInFilePath = Directory.GetFiles(filePath, "*.docx");
+                AllFiles.AddRange(filesInFilePath);
+            }
+
+            return files;
+        }
+
+        /// <summary>
+        /// Validate and create a directory
+        /// </summary>
+        /// <param name="dir"></param>
+        private void CreateDirectoryIfNotExist(string dir)
+        {
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+        }
+
+        /// <summary>
+        /// validate and create a file
+        /// </summary>
+        /// <param name="file"></param>
+        private void CreateFileIfNotExist(string file)
+        {
+            if (!File.Exists(file))
+            {
+                var createFile = File.Create(file);
+                createFile.Close();
+            }
+        }
+        #endregion
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Office.Interop.Word;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System;
 using System.Linq;
 using System.Threading;
 
@@ -11,93 +8,40 @@ namespace Doc_Merger
     {
         static void Main(string[] args)
         {
-            killAllWordProcess();
-            Paths paths = new Paths();
-            List<string> allFilesPath = default;
-            if (args.Length > 0)
-                allFilesPath.AddRange(args);
+            KillAllWordProcesses();
 
-            paths.AllFilePath.AddRange(allFilesPath);
-            string currentDir = Directory.GetCurrentDirectory();
-            string finalPath = currentDir + @"\Results";
-            string finalFile = finalPath + @"\Completed\Result.docx";
-            string errorFile = finalPath + @"\Errors\errors.txt";
-            ValidatePaths(currentDir, finalPath, finalFile, errorFile);
+            Paths allPaths;
 
-            var t1 = new Thread(new ThreadStart(() => startup(allFilePath[0], finalPath[0], errorLogPath[0])));
-            var t2 = new Thread(new ThreadStart(() => startup(allFilePath[1], finalPath[1], errorLogPath[1])));
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("I need some files.\n Run this tool again with the path to the files as arguments");
+                Console.ReadKey();
+                return;
+            }
+
+            allPaths = new Paths(args.ToList());
+            if (!allPaths.CreateAndValidatePaths())
+            {
+                Console.WriteLine("something went wrong!\nBreaking execution...");
+                Console.ReadKey();
+                return;
+            }
+
+            var maxThreads = Environment.ProcessorCount;
+
+            var t1 = new Thread(new ThreadStart(() => allPaths.Execute()));
+            var t2 = new Thread(new ThreadStart(() => allPaths.Execute()));
 
             t1.Start();
             t2.Start();
         }
 
-        private static void ValidatePaths(string currentDir, string finalPath, string finalFile, string errorFile)
-        {
-            if (!Directory.Exists($"{ currentDir }\\Results"))
-                Directory.CreateDirectory($"{currentDir}\\Results");
-
-            if (!Directory.Exists($"{ finalPath }\\Completed"))
-                Directory.CreateDirectory($"{finalPath}\\Completed");
-            if (!Directory.Exists($"{ finalPath }\\Errors"))
-                Directory.CreateDirectory($"{finalPath}\\Errors");
-
-            if (!File.Exists(finalFile))
-                File.Create(finalFile);
-            if (!File.Exists(errorFile))
-                File.Create(errorFile);
-        }
-
-        private static void startup(string allFilesPath, string finalPath, string errorLogPath)
-        {
-            var app = new Application();
-            var files = Directory.GetFiles(allFilesPath, "*.docx");
-            CreateFileIfNotExist(errorLogPath);
-            CreateFileIfNotExist(finalPath);
-
-            var finalFile = app.Documents.Open(finalPath);
-            var count = 1;
-            finalFile.PageSetup.Orientation = WdOrientation.wdOrientLandscape;
-            files.ToList().ForEach(file =>
-            {
-                try
-                {
-                    finalFile.ActiveWindow.Selection.InsertFile(file);
-                    finalFile.Save();
-                    Console.WriteLine($"{count} - {file}\n");
-                }
-                catch (System.Runtime.InteropServices.COMException)
-                {
-                    using (StreamWriter writer = File.CreateText(errorLogPath))
-                    {
-                        writer.WriteLine($"{count} - {file}\n");
-                    }
-                }
-                count++;
-
-
-            });
-
-            finalFile.Close();
-            app.Quit();
-        }
-
-        private static void killAllWordProcess()
+        private static void KillAllWordProcesses()
         {
             String comand = "/C taskkill /f /IM WINWORD.EXE";
             System.Diagnostics.Process.Start("cmd.exe", comand);
             Console.WriteLine("press any key to start!\n");
             Console.ReadKey();
-        }
-
-        private static void CreateFileIfNotExist(string finalPath)
-        {
-            if (!File.Exists(finalPath))
-            {
-                var file = File.Create(finalPath);
-                file.Close();
-
-            }
-
         }
     }
 }
